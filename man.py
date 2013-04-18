e35030325d80ae4a13d60d04fd0bafb435132e22
mcieminski <mitchell.cieminski@gmail.com>
2013-04-18 17:02:04 -0400

    I made the man.py file, the code in which will end up being the entire game. It will end up using arrow keys to handle movement of the pacman dude.

--- man.py
+++ man.py
@@ -1,50 +1,71 @@
+import os, sys
+import pygame
+from pygame.locals import *
+
+if not pygame.font:
+    print 'Warning, fonts disabled'
+if not pygame.mixer:
+    print 'Warning, sounds disabled'
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error, message:
+        print('Cannot load image:', name)
+        raise SystemExit, message
+    image = image.convert
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0,0))
+        image.set_colorkey(colorkey, RLEACCEL)
+    return image, image.get_rect()
+
+def load_sound(name):
+    class NoneSound:
+        def play(self):
+            pass
+    if not pygame.mixer:
+        return NoneSound()
+    fullname = os.path.join('data', name)
+    try:
+        sound = pygame.mixer.Sound(fullname)
+    except pygame.error, message:
+        print 'Cannot load sound:', wav)
+        raise SystemExit, message
+    return sound
+
 from math import sqrt
+#get the key in the dictionary from the value
 def reverse_lookup(d, v):
     for k in d:
         if d[k] == v:
             return k
     raise ValueError
 
-class pacman(object):
+#Defines the generic playable character object
+class dude(object):
+    #Dictionary to go from direction to direction unit vector
     directions = {'up': [0,1], 'left': [0,-1],'down': [0,-1],'right':[1,0]}
-    def __init__(self, name = 'pacman', position = [0,0], nextpos = [0,1], target = [-1,-1], vhat = [0,1], speed =10):
-        self.name = name
+    def __init__(self, name = 'Pacman', position = [0,0], nextpos = [0,1], vhat = [-1,0], speed = 10):
+	#Initialize ghost parameters
+        pygame.sprite.Sprite.__init__(self) #call sprite initializer
+        self.image, self.rect = load_image('pacman.bmp',-1)
         self.position = position
-        self.target = target
-        self.chase = chase
-        self.speed = speed
-        self.vhat = vhat
         self.nextpos = nextpos
-    def __str__(self):
-        namestr = self.name.upper()
-        posstr = 'position:  ' + str(self.position)
-        targetstr = 'target:    ' + str(self.target) 
-        speedstr = 'speed:     ' + str(self.speed)
-        directionstr = 'direction: ' + str(reverse_lookup(self.directions, self.vhat))
-        nextposstr = 'next pos:  ' + str(self.nextpos)
-        chasestr = 'chase?     ' + str(self.chase)
-        res = []
-        res.append(namestr)
-        res.append(posstr)
-        res.append(targetstr)
-        res.append(speedstr)
-        res.append(directionstr)
-        res.append(nextposstr)
-        res.append(chasestr)
-        
-        return '\n'.join(res)
-    def new_next_pos(self):
-        possmoves = [[0,2],[1,1],[0,0],[-1,1]] 
-        possmoves.remove(self.position)
-        possdist = []
-        for move in possmoves:
-            possdist.append(sqrt((self.target[0] - move[0])**2 + (self.target[1] - move[1])**2))
-        leastdistindex = 0
-        for d in range(len(possdist)):
-            if possdist[d] < possdist[leastdistindex]:
-                leastdist = d        
+        self.vhat = vhat
+        self.speed = speed
+    def update(self):
+        #move the dood
+        self.shipRect.center = (
 
-Robert = Ghost('Robert')
-print(Robert)
-Robert.new_next_pos()
-        
+
+py.init()
+screen = pygame.display.set_mode((500,500))
+pygame.display.set_caption('pacman')
+pygame.mouse.set_visible(0)
+
+background = pygame.Surface(sceen.get_size())
+background = background.convert()
+background.fill = ((0,0,0))
+    
