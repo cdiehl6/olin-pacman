c6710fbdf7c76334c87d5e15b3b31faac2749798
Claire Diehl <cdiehl6.gmail.com>
2013-04-28 15:38:38 -0400

    add trialmap.py. partially working.

--- trialmap.py
+++ trialmap.py
@@ -5,20 +5,25 @@ import ConfigParser
 def load_tile_table(filename, width, height):
     image = pygame.image.load(filename).convert()
     image_width, image_height = image.get_size()
+    print(image_width)
+    print(image_height)
     tile_table = []
     for tile_x in range(0, image_width/width):
+        print('hi')
         line = []
         tile_table.append(line)
         for tile_y in range(0, image_height/height):
             rect = (tile_x*width, tile_y*height, width, height)
+            print(rect)
             line.append(image.subsurface(rect))
     return tile_table
 
 if __name__=='__main__':
     pygame.init()
-    screen = pygame.display.set_mode((450,522))
+    screen = pygame.display.set_mode((432,522))
     screen.fill((255, 255, 255))
     table = load_tile_table("pacmantiles.png", 18, 18)
+    print (list (enumerate(table)))
     for x, row in enumerate(table):
         for y, tile in enumerate(row):
             screen.blit(tile, (x*18, y*18))
@@ -32,7 +37,7 @@ class Level(object):
         self.key={}
         parser=ConfigParser.ConfigParser()
         parser.read(filename)
-        self.tileset=parser.get("level","map").split("\n")
+        self.tileset=parser.get("Level","map").split("\n")
         print('self.tileset')
         if len(section)==1:
             desc=dict(parser.items(section))
@@ -74,3 +79,37 @@ class Level(object):
         tile_image=tiles[tile[0]][tile[1]]
         image.blit(tile_image,(map_x*18,map_y*18))
         return image, overlays
+if __name__ == "__main__":
+    screen = pygame.display.set_mode((424, 320))
+
+    MAP_TILE_WIDTH = 18
+    MAP_TILE_HEIGHT = 18
+    MAP_CACHE = {'pacmantiles.png': load_tile_table('pacmantiles.png', MAP_TILE_WIDTH,MAP_TILE_HEIGHT)}
+
+    level = Level()
+    level.load_file('level.map')
+
+    clock = pygame.time.Clock()
+
+    background, overlay_dict = level.render()
+    overlays = pygame.sprite.RenderUpdates()
+    for (x, y), image in overlay_dict.iteritems():
+        overlay = pygame.sprite.Sprite(overlays)
+        overlay.image = image
+        overlay.rect = image.get_rect().move(x * 24, y * 16 - 16)
+    screen.blit(background, (0, 0))
+    overlays.draw(screen)
+    pygame.display.flip()
+game_over = False
+while not game_over:
+
+    # XXX draw all the objects here
+
+    overlays.draw(screen)
+    pygame.display.flip()
+    clock.tick(15)
+    for event in pygame.event.get():
+        if event.type == pygame.locals.QUIT:
+            game_over = True
+        elif event.type == pygame.locals.KEYDOWN:
+            pressed_key = event.key
